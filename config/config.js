a9efb47699e3c03a8a9bc1648e9f406afe31c36a
arip.budiman@getrupi.com <Arip Budiman>
2024-04-12 05:44:16 +0700

    refactor:config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv').config().parsed;
 
 module.exports ={
   "development": {
-    "username": dotenv.DB_USER,
+    "username": dotenv.DB_USERNAME,
     "password": dotenv.DB_PASSWORD,
     "database": dotenv.DB_NAME,
     "host": dotenv.DB_HOST,
