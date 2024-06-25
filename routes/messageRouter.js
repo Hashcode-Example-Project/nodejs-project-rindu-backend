83eab9a09059ea2ce950d42f0ccaf6f3f1b8fd94
Arip Budiman <aripbudiman007@gmail.com>
2024-06-26 04:54:20 +0700

    chore: message routes

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,9 +1,10 @@
-// user.js
 const express = require('express');
-const { getAllMessage } = require('../controllers/MessageController.js');
+const { getAllMessage, getMessageById, updateMessageStatus } = require('../controllers/MessageController.js');
 
 const router = express.Router();
 
 router.get("/", getAllMessage);
+router.get("/:id", getMessageById);
+router.patch("/selected/:id", updateMessageStatus);
 
 module.exports = router;
